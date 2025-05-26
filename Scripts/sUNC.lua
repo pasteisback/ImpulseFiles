d160700fd15b37cfaf1b51ed9930ef0f2b4095cd
Cactus <roccoagiacomino@gmail.com>
2025-05-26 15:58:46 -0500

    Create sUNC.lua

---
+++ Scripts/sUNC.lua
@@ -0,0 +1,17 @@
+local aliteralbaseplateJoinID = 4483381587
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Impulse",
+	Text = "IMPULSE ON TOP! dsc.gg/impulserbx",
+	Duration = 9
+})
+if not in aliteralbaseplateJOinID then
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Impulse",
+	Text = "I reccomend joining a literal baseplate to get the best results. Would you like to join?",
+	Duration = 9
+    Button = "Yes", "no"
+    Yes.Function:Connect()
+    Teleport:aliteratJoinID()
+  end
+})
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HummingBird8/HummingRn/main/sUNCTestGET"))()
